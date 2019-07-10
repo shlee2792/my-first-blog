e72ab62c86b570c6fe5e1291de5a499d94cf6a3c
shlee2792 <shlee2792@naver.com>
2019-07-10 14:54:38 +0900

    Modified templates to display posts from database

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
         blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.new()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
